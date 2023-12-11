4122d1ee1697693cf559875dba0b40ae0a3e2f30
Roman Tolmachev <tolmachevroman@gmail.com>
2023-12-11 13:03:43 -0300

    add packages section

---
+++ demo_reader/multireader.py
@@ -0,0 +1,20 @@
+import os
+from demo_reader.compressed import bzipped, gzipped
+
+extension_map = {
+    '.bz2': bzipped.open,
+    '.gz': gzipped.open,
+}
+
+
+class MultiReader:
+    def __init__(self, filename):
+        extension = os.path.splitext(filename)[1]
+        opener = extension_map.get(extension, open)
+        self.f = opener(filename, 'rt')
+
+    def close(self):
+        self.f.close()
+
+    def read(self):
+        return self.f.read()
